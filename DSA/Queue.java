02cc97bab4bcc7a82eb31e475b46bbf24a0d8830
AmanKumar9122 <amanraj15112003@gmail.com>
2024-02-18 14:15:43 +0530

    Add

--- DSA/Queue.java
+++ DSA/Queue.java
@@ -8,80 +8,53 @@ class Queue {
 		capacity = c;
 		queue = new int[capacity];
 	}
-
-	// function to insert an element
-	// at the rear of the queue
 	static void queueEnqueue(int data)
 	{
-		// check queue is full or not
 		if (capacity == rear) {
 			System.out.printf("\nQueue is full\n");
 			return;
 		}
-
-		// insert element at the rear
 		else {
 			queue[rear] = data;
 			rear++;
 		}
 		return;
 	}
-
-	// function to delete an element
-	// from the front of the queue
-	static void queueDequeue()
-	{
-		// if queue is empty
-		if (front == rear) {
-			System.out.printf("\nQueue is empty\n");
-			return;
-		}
-
-		// shift all the elements from index 2 till rear
-		// to the right by one
-		else {
-			for (int i = 0; i < rear - 1; i++) {
-				queue[i] = queue[i + 1];
-			}
-
-			// store 0 at rear indicating there's no element
-			if (rear < capacity)
-				queue[rear] = 0;
-
-			// decrement rear
-			rear--;
-		}
-		return;
-	}
-
-	// print queue elements
-	static void queueDisplay()
-	{
+	// static void queueDequeue(){
+	// 	if (front == rear) {
+	// 		System.out.printf("\nQueue is empty\n");
+	// 		return;
+	// 	} else {
+	// 		for (int i = 0; i < rear - 1; i++) {
+	// 			queue[i] = queue[i + 1];
+	// 		}
+	// 		if (rear < capacity)
+	// 			queue[rear] = 0;
+	// 		rear--;
+	// 	}
+	// 	return;
+	// }
+	static void queueDisplay(){
 		int i;
 		if (front == rear) {
 			System.out.printf("\nQueue is Empty\n");
 			return;
 		}
-
-		// traverse front to rear and print elements
 		for (i = front; i < rear; i++) {
 			System.out.printf(" %d <-- ", queue[i]);
 		}
 		return;
 	}
-
-	// print front of queue
-	static void queueFront()
-	{
-		if (front == rear) {
-			System.out.printf("\nQueue is Empty\n");
-			return;
-		}
-		System.out.printf("\nFront Element is: %d",
-						queue[front]);
-		return;
-	}
-	// Driver code
+	// static void queueFront()
+	// {
+	// 	if (front == rear) {
+	// 		System.out.printf("\nQueue is Empty\n");
+	// 		return;
+	// 	}
+	// 	System.out.printf("\nFront Element is: %d",
+	// 					queue[front]);
+	// 	return;
+	// }
 	public static void main(String[] args)
 	{
 		// Create a queue of capacity 4
@@ -105,15 +78,15 @@ class Queue {
 		// print Queue elements
 		q.queueDisplay();
 
-		q.queueDequeue();
-		q.queueDequeue();
-		System.out.printf(
-			"\n\nafter two node deletion\n\n");
+		// q.queueDequeue();
+		// q.queueDequeue();
+		// System.out.printf(
+		// 	"\n\nafter two node deletion\n\n");
 
-		// print Queue elements
-		q.queueDisplay();
+		// // print Queue elements
+		// q.queueDisplay();
 
 		// print front of the queue
-		q.queueFront();
+		//q.queueFront();
 	}
 }
