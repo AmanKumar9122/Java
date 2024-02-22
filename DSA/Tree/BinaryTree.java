3ee3a03ddc390776a032bc9ffe89bf54133fcff1
AmanKumar9122 <amanraj15112003@gmail.com>
2024-02-22 19:22:38 +0530

    added codes

--- DSA/Tree/BinaryTree.java
+++ DSA/Tree/BinaryTree.java
@@ -1,6 +1,4 @@
-import java.util.LinkedList;
-import java.util.Queue;
-
+import java.util.*;
 public class BinaryTree {
     static class Node {
         int data;
@@ -13,10 +11,8 @@ public class BinaryTree {
             this.right = null;
         }
     }
-
     static class Btree {
         static int idx = -1;
-
         public static Node buildTree(int nodes[]) {
             idx++;
             if (nodes[idx] == -1) {
@@ -27,7 +23,6 @@ public class BinaryTree {
             newNode.right = buildTree(nodes);
             return newNode;
         }
-
         public static void preorder(Node root) {
             if (root == null) {
                 return;
@@ -88,8 +83,76 @@ public class BinaryTree {
         int rh = height(root.right);
         return Math.max(lh, rh) + 1 ;
         }
-    }
+        public static int count(Node root){
+            if(root==null){
+                return 0;
+            }
+            int lcount = count(root.left);
+            int rcount = count(root.right);
+            return lcount+rcount+1;
+        }
+        public static int sum(Node root){
+            if(root==null){
+                return 0;
+            }
+            int lsum = sum(root.left);
+            int rsum = sum(root.right);
+            return lsum + rsum + root.data;
+        }
+        public static int diameter(Node root){  //time complexity= O(n^2)
+            if(root==null){
+                return 0;
+            }
+            int ldiam = diameter(root.left);
+            int rdiam = diameter(root.right);
+            int lh = height(root.left);
+            int rh = height(root.right);
+            int selfdiam = lh + rh +1;
+            return Math.max (Math.min(ldiam,rdiam), selfdiam);
+        }
+        static class Info{
+            int diam;
+            int ht;
 
+            public Info(int diam,int ht){
+                this.diam=diam;
+                this.ht=ht;
+            }
+            public static Info diameter2(Node root){ //time complexity= O(n)
+                if(root==null){
+                    return new Info(0, 0);
+                }
+                Info leftInfo = diameter2(root.left);
+                Info rightInfo = diameter2(root.right);
+                int diam = Math.max(Math.max(leftInfo.diam,rightInfo.diam),leftInfo.ht+rightInfo.ht+1);
+                int ht = Math.max(leftInfo.ht,rightInfo.ht) + 1;
+                return new Info(diam, ht);
+            }
+        }
+        public static boolean isIdentical(Node node,Node subRoot){
+            if(node==null&subRoot==null){
+                return true;
+            }else if(node==null||subRoot==null|| node.data!=subRoot.data){
+                return false;
+            }
+            if(!isIdentical(node.left, subRoot.left)){
+                return false;
+            }
+            if(!isIdentical(node.right, subRoot.right)){
+                return false;
+            }
+            return true;
+            }
+        public static boolean isSubtree(Node root,Node subRoot){
+            if(root==null){
+                return false;
+            }
+            if(root.data == subRoot.data){
+                return true;
+            }
+            return isSubtree(root.left, subRoot.left)||isSubtree(root.right, subRoot.right);
+        }
+    }
     public static void main(String[] args) {
         int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
         Btree tree = new Btree();
@@ -104,5 +167,16 @@ public class BinaryTree {
         tree.levelOrder(root);
         System.out.println();
         System.out.println(tree.height(root));
+        System.out.println();
+        System.out.println(tree.count(root));
+        System.out.println();
+        System.out.println(tree.sum(root));
+        System.out.println();
+        System.out.println(tree.diameter(root));
+        System.out.println();
+        Btree.Info info = new Btree.Info(0, 0);
+        System.out.println(info.diameter2(root).diam);
+        System.out.println(info.diameter2(root).ht);
+
     }
 }
