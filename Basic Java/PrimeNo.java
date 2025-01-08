92f0ea5d3f28cb5ca1c1e48c5fa9205502193775
AmanKumar9122 <amanraj15112003@gmail.com>
2025-01-08 10:25:43 +0530

    push

---
+++ Basic Java/PrimeNo.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class PrimeNo {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value :");
+        int x = sc.nextInt(); // Changed to int for proper modulus operation
+        for (int i = 1; i <= Math.sqrt(x); i++) { // Start from 1 to avoid division by 0
+            if (x % i == 0) { // Check if i is a factor
+                list.add(i);
+                if (i != x / i) { // Avoid adding the same factor twice
+                    list.add(x / i);
+                }
+            }
+        }
+        if(list.size()==2){
+            System.out.println("true");
+        }else{
+            System.out.println("false");
+        }
+    }
+}
