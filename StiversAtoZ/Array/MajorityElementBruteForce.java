d7fe8de13ef3950402f5a2a4289b73450c6c21bd
AmanKumar9122 <amanraj15112003@gmail.com>
2025-02-22 10:26:05 +0530

    brute

---
+++ StiversAtoZ/Array/MajorityElementBruteForce.java
@@ -0,0 +1,21 @@
+public class MajorityElementBruteForce {
+        public int majorityElement(int[] arr) {
+            int n = arr.length;
+            for(int i=0;i<n;i++){
+                int count=0;
+                for(int j=0;j<n;j++){
+                    if(arr[i]==arr[j]){
+                        count++;
+                    }
+                }
+                if(count>n/2) return arr[i];
+            }
+            return -1;
+        }
+        public static void main(String[] args) {
+            MajorityElementBruteForce obj = new MajorityElementBruteForce();
+            int[] arr = {3,2,3};
+            System.out.println(obj.majorityElement(arr));
+             
+        }
+}
