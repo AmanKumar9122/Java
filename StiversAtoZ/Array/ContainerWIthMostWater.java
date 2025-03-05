8e7499ba27bef298ba456cc894ddaad6cf758aac
AmanKumar9122 <amanraj15112003@gmail.com>
2025-03-05 10:40:24 +0530

    push

--- StiversAtoZ/Array/ContainerWIthMostWater.java
+++ StiversAtoZ/Array/ContainerWIthMostWater.java
@@ -1,18 +1,45 @@
 //optimized solution
+// public class ContainerWIthMostWater {
+//     public int maxArea(int[] height) {
+//         int left = 0, right = height.length - 1;
+//         int maxA = 0;
+        
+//         while (left < right) {
+//             int area = Math.min(height[left], height[right]) * (right - left);
+//             maxA = Math.max(maxA, area);
+            
+//             // Move the pointer with the smaller height
+//             if (height[left] < height[right]) {
+//                 left++;
+//             } else {
+//                 right--;
+//             }
+//         }
+        
+//         return maxA;
+//     }    
+//     public static void main(String[] args) {
+//         int[] height = {1,8,6,2,5,4,8,3,7};
+//         ContainerWIthMostWater obj = new ContainerWIthMostWater();
+//         System.out.println(obj.maxArea(height));  
+//     }
+// }
+
+// Complexity Analysis:
+// Time Complexity: O(n), where n is the size of the input array.
+// Space Complexity: O(1). The space complexity is O(1) because we are using a constant amount of extra space for the loop variables.
+
+
+//Brute Force Solution
 public class ContainerWIthMostWater {
     public int maxArea(int[] height) {
-        int left = 0, right = height.length - 1;
+        int n = height.length;
         int maxA = 0;
         
-        while (left < right) {
-            int area = Math.min(height[left], height[right]) * (right - left);
-            maxA = Math.max(maxA, area);
-            
-            // Move the pointer with the smaller height
-            if (height[left] < height[right]) {
-                left++;
-            } else {
-                right--;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int area = Math.min(height[i], height[j]) * (j - i);
+                maxA = Math.max(maxA, area);
             }
         }
         
@@ -20,11 +47,11 @@ public class ContainerWIthMostWater {
     }    
     public static void main(String[] args) {
         int[] height = {1,8,6,2,5,4,8,3,7};
-        ContainerWIthMostWater obj = new ContainerWIthMostWater(); // Create an instance
-        System.out.println(obj.maxArea(height));  // Call method using the instance
+        ContainerWIthMostWater obj = new ContainerWIthMostWater();
+        System.out.println(obj.maxArea(height));  
     }
 }
 
 // Complexity Analysis:
-// Time Complexity: O(n), where n is the size of the input array.
+// Time Complexity: O(n^2), where n is the size of the input array.
 // Space Complexity: O(1). The space complexity is O(1) because we are using a constant amount of extra space for the loop variables.
