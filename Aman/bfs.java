5b7b584b87d3c25042ac04eda447e65a52b3d239
AmanKumar9122 <amanraj15112003@gmail.com>
2025-06-26 15:44:31 +0530

    updated questions

--- Aman/bfs.java
+++ Aman/bfs.java
@@ -47,54 +47,47 @@ import java.util.*;
 //time complexity: O(V + E)
 //space complexity: O(V + E) for the adjacency list and O(V) for the visited array and queue
 
-// count the number of connected components in a graph
+// bfs on a given 2d matrix
 public class bfs {
-    public int countComponents(int v, int[][] edges) {
-        List<List<Integer>> adj = new ArrayList<>();
-        for (int i = 0; i < v; i++) {
-            adj.add(new ArrayList<>());
-        }
+    public void bfs(int[][] grid, int startRow, int startCol) {
+        int rows = grid.length;
+        int cols = grid[0].length;
+        boolean[][] visited = new boolean[rows][cols];
+        Queue<int[]> queue = new LinkedList<>();
+        ArrayList<int[]> bfsOrder = new ArrayList<>();
 
-        for (int[] edge : edges) {
-            int u = edge[0];
-            int v1 = edge[1];
-            adj.get(u).add(v1);
-            adj.get(v1).add(u);
-        }
-
-        boolean[] visited = new boolean[v];
-        int count = 0;
-
-        for (int i = 0; i < v; i++) {
-            if (!visited[i]) {
-                bfsUtil(i, adj, visited);
-                count++;
-            }
-        }
-
-        return count;
-    }
-
-    private void bfsUtil(int start, List<List<Integer>> adj, boolean[] visited) {
-        Queue<Integer> queue = new LinkedList<>();
-        queue.add(start);
-        visited[start] = true;
+        queue.add(new int[]{startRow, startCol});
+        visited[startRow][startCol] = true;
 
         while (!queue.isEmpty()) {
-            int node = queue.poll();
+            int[] cell = queue.poll();
+            bfsOrder.add(cell);
+            int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+            for (int[] dir : directions) {
+                int newRow = cell[0] + dir[0];
+                int newCol = cell[1] + dir[1];
 
-            for (int neighbor : adj.get(node)) {
-                if (!visited[neighbor]) {
-                    visited[neighbor] = true;
-                    queue.add(neighbor);
+                if (newRow >= 0 && newRow < rows && newCol >= 0 && newCol < cols && !visited[newRow][newCol]) {
+                    visited[newRow][newCol] = true;
+                    queue.add(new int[]{newRow, newCol});
                 }
             }
         }
+
+        System.out.print("BFS Order: ");
+        for (int[] cell : bfsOrder) {
+            System.out.print(grid[cell[0]][cell[1]] + " ");
+        }
+        System.out.println();
     }
 
     public static void main(String[] args) {
-        int[][] edges = {{0, 1}, {1, 2}, {2, 6}, {0, 7}, {7, 3}, {3, 6}, {0, 9}, {9, 5}, {5, 6}};
+        int[][] grid = {
+            {0, 1, 0},
+            {1, 0, 1},
+            {0, 1, 0}
+        };
         bfs g = new bfs();
-        System.out.println("Number of connected components: " + g.countComponents(10, edges));
+        g.bfs(grid, 1, 1); // Starting from cell (1, 1)
     }
 }
